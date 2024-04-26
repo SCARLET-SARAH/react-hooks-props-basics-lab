74eb384725a6f2e817761b8a114ffcdeb8effa3b
scarlet <sarahscarlet641@gmail.com>
2024-04-26 07:35:26 +0300

    cpmlete

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,20 @@ function About() {
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={github} linkedin={linkedin} />
+      function Links({ github, linkedin }) {
+           return (
+      <div>
+          <h3>Links</h3>
+          <a href={github}>{github}</a>
+          <a href={linkedin}>{linkedin}</a>
+      </div>
+      
+  );
+}
     </div>
   );
+
 }
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,13 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+const user = {
+  name: 'Liza',
+  bio: 'I am a web developer',
+  github: 'https://github.com/liza',
+  linkedin: 'https://www.linkedin.com/in/liza/'
+};
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
@@ -11,8 +18,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} />
+      <About bio={user.bio} github={user.github} linkedin={user.linkedin}  />
     </div>
   );
 }
